914f0b0806ceb277387a666895a80fd4819a4811
IoannaThoma <ioanna.thoma@ed.ac.uk>
2023-04-21 19:48:32 +0100

    Drift ROC curves

--- Figures/Fig_4a.R
+++ Figures/Fig_4a.R
@@ -1,5 +1,6 @@
 # List of libraries to check and install
-libs <- c("tidyverse", "patchwork", "viridis", "ggdist")
+libs <- c("tidyverse", "patchwork", "ggdist", "dplyr", "tidyr",
+          "forcats", "ggrepel", "purrr")
 
 # Create an empty list to store the missing libraries
 missing_libs <- list()
@@ -40,38 +41,62 @@ pdf(paste0(plot_dir,"cohort/drift/drift_roc.pdf"),width=3,height=3.5)
 roc_2panel(list(xy1,xy2,xy3,xy4,xy5),labels=as.character(test_times),col=gray(((1:ntx)-1)/(1.3*ntx)))
 dev.off()
 
-# code converted to use ggplot2 for creating ROC curves for a predictor at various time points:
+# use ggplot2 to create ROC curves for a predictor at various time points
 
-# Combine ROC data into a single data frame
+# The following code uses the default colours in ggplot2.
+# It also creates the lower panel with the differences wrt to the curve
+# corresponding to the first time point
+
+# ROC curves for each time point
+rocs <- list(xy1, xy2, xy3, xy4, xy5)
+
+# Generate colors
+colors <- scales::hue_pal()(length(rocs))
+
+# Combine the ROC curves into a single data frame
 df <- data.frame(
-  Model = rep(as.character(test_times), each = length(xy1$sens)),
+  Time = rep(as.character(test_times), each = length(xy1$sens)),
   Sensitivity = c(xy1$sens, xy2$sens, xy3$sens, xy4$sens, xy5$sens),
-  Specificity = 1 - c(xy1$spec, xy2$spec, xy3$spec, xy4$spec, xy5$spec)
+  Specificity = c(1 - xy1$spec, 1 - xy2$spec, 1 - xy3$spec, 1 - xy4$spec,
+                  1 - xy5$spec)
 )
 
 # Convert test_times to character
 test_times <- as.character(test_times)
 
-# Define custom colours
-# viridis colour palette is known to be colour-blind friendly
-# Define a custom gradient color scale centered on blue
-colors <- viridis(5, begin = 0.3, end = 0.7, direction = -1)
-
-# Create ggplot object
-p <- ggplot(df, aes(x = 1 - Specificity, y = Sensitivity, color = Model, linetype = Model)) +
+# Create the ROC plot
+roc_plot <- ggplot(df, aes(x = 1 - Specificity, y = Sensitivity, color = Time, linetype = Time)) +
   geom_line(linetype = "solid") +
-  scale_color_manual(values = colors) + # manually specify colours
+  scale_color_manual(values = colors) +
   labs(x = "1 - Specificity", y = "Sensitivity") +
-  theme_minimal() +
-  geom_abline(slope = -1, intercept = 1, linetype = "dashed", color = "gray") # Add dashed gray line
+  theme_minimal(base_size = 8) +
+  geom_abline(slope = -1, intercept = 1, linetype = "dashed", color = "gray")
 
-# Print the ggplot object
-print(p)
+# extract spec column from each roc object
+spec_list <- lapply(rocs, function(x) x$spec)
+
+# subtract the first roc's spec from each other roc's spec
+diffs_list <- lapply(spec_list[-1], function(x) rocs[[1]]$spec - x)
+
+# combine the diffs and add a Time column
+Time <- rep.int(rep(test_times[2:5], each = 100), length(diffs_list))
+diffs <- data.frame(
+              Delta = unlist(diffs_list),
+              Specificity = rep(rocs[[1]]$spec, length(diffs_list)),
+              Time = Time)
+
+# Create the difference plot
+diff_plot <- ggplot(diffs, aes(x = 1 - Specificity, y = Delta, color = Time, linetype = Time)) +
+  geom_line(linetype = "solid") +
+  scale_color_manual(values = colors[2:5]) +
+  labs(x = "1 - Specificity", y = expression(paste(Delta, " Sensitivity"))) +
+  theme_minimal(base_size = 8) +
+  geom_abline(slope = 0, intercept = 0, linetype = "dashed", color = "gray")
+
+# Combine the two plots vertically - alternative heights = c(3, 1)
+print(roc_plot / diff_plot) + plot_layout(heights = c(2/3, 1/3))
 
 # Save the plot to a file
 ggsave("Figures/pdfs/Fig_4a.pdf",
-        width = 7.5, height = 7.25, units = "cm",
-        device = cairo_pdf)
-
-
-
+       width = 7.5, height = 7.25, units = "cm",
+       device = cairo_pdf)
