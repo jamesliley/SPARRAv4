2b70213e1f81d47a744714081ebab14340327959
IoannaThoma <ioanna.thoma@ed.ac.uk>
2023-06-05 20:08:42 +0300

    Performance by v3 cohorts

--- Figures/Fig_3c.R
+++ Figures/Fig_3c.R
@@ -45,23 +45,24 @@ perfmin <- min(perf[1:m0, ]) - 0.02
 
 # Create a data frame for plotting
 plot_data <- data.frame(
-  x = rep((m0 + 2) * (1:n0) - floor(m0 / 2) - 1, each = m0),
-  y = c(t(perf[1:m0, ])),
+  x = factor(rep((m0 + 2) * (1:n0) - floor(m0 / 2) - 1, each = m0)),
+  y = c(perf[1:m0, ], perf[1:m0, ]),
   group = factor(rep(1:n0, each = m0)),
   version = rep(c("V4", "V3"), times = n0)
 )
 
 # Set up the plot
-p3c <-
-  ggplot(plot_data, aes(x = x, y = y, group = group)) +
+p3c1_min <-
+  ggplot(plot_data, aes(x = x, y = y, group = version)) +
   geom_point(aes(color = version), shape = 18, size = 3) +
-  scale_x_continuous(
+  scale_color_manual(values = c("V4" = "black", "V3" = "red")) +
+  scale_x_discrete(
     breaks = (m0 + 2) * (1:n0) - floor(m0 / 2) - 1,
     labels = labs,
-    expand = c(0, 0), limits = c(0, 15),  # Adjust the expand argument to extend the x-axis limits
+    #expand = c(0, 0), limits = c(0, 15),  # Adjust the expand argument to extend the x-axis limits
     position = "bottom"
   ) +
-  scale_y_continuous(expand = c(0, 0), limits = c(0, 1)) +  # Extend the y-axis to 1
+  #scale_y_continuous(expand = c(0, 0), limits = c(0, 1)) +  # Extend the y-axis to 1
   labs(x = "Cohort", y = "AUROC") +
   theme_bw() +
   theme(
@@ -72,6 +73,65 @@ p3c <-
   )
 
 
-ggsave("Figures/pdfs/Fig_3c.pdf", p3c,
+ggsave("Figures/pdfs/Fig_3c1.pdf", p3c1_min,
+       width = 8.5, height = 9, units = "cm",
+       device = cairo_pdf)
+
+
+# difference in AUC
+plot_data <- data.frame(
+  x = factor(rep((m0 + 2) * (1:n0) - floor(m0 / 2) - 1, each = 1)),
+  y = c(perf[1:m0, ], perf[1:m0, ]),
+  group = factor(rep(1:n0, each = m0)),
+  version = rep(c("V4", "V3"), times = n0)
+)
+plot_data$difference <- plot_data$y[plot_data$version == "V4"] -
+  plot_data$y[plot_data$version == "V3"]
+
+# Set up the plot
+p3c2 <-
+  ggplot(plot_data, aes(x = x, y = difference, group = version)) +
+  geom_point(shape = 18, size = 3) +
+  scale_x_discrete(
+    breaks = (m0 + 2) * (1:n0) - floor(m0 / 2) - 1,
+    labels = labs,
+    position = "bottom") +
+  xlab("Cohort") +
+  ylab("Difference in AUC") +
+  theme_bw() +
+  theme(
+    legend.position = "bottom",
+    legend.title = element_blank(),
+    axis.text.x = element_text(angle = 45, hjust = 1)
+  )
+
+ggsave("Figures/pdfs/Fig_3c2.pdf", p3c2,
+       width = 8.5, height = 9, units = "cm",
+       device = cairo_pdf)
+
+
+
+# Percentage increase
+plot_data$percentage_increase <- ((plot_data$y[plot_data$version == "V4"] / plot_data$y[plot_data$version == "V3"]) - 1) * 100
+
+
+p3c3 <-
+  ggplot(plot_data, aes(x = x, y = percentage_increase)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  xlab("Cohort") +
+  ylab("Percentage Increase") +
+  scale_x_discrete(
+    breaks = (m0 + 2) * (1:n0) - floor(m0 / 2) - 1,
+    labels = labs,
+    position = "bottom") +
+  theme_bw() +
+  theme(
+    legend.position = "bottom",
+    legend.title = element_blank(),
+    axis.text.x = element_text(angle = 45, hjust = 1)
+  )
+
+
+ggsave("Figures/pdfs/Fig_3c3.pdf", p3c3,
        width = 8.5, height = 9, units = "cm",
        device = cairo_pdf)
