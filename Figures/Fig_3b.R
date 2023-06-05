3e516989284534fafd34ebd0178b5abc000a98a9
IoannaThoma <ioanna.thoma@ed.ac.uk>
2023-06-05 20:09:41 +0300

    Performance by SIMD

--- Figures/Fig_3b.R
+++ Figures/Fig_3b.R
@@ -46,17 +46,19 @@ perfmin <- min(perf[1:m0, ]) - 0.02
 # Create a data frame for plotting
 plot_data <- data.frame(
   x = factor(rep((m0 + 2) * (1:n0) - floor(m0 / 2) - 1, each = m0)),
-  y = c(t(perf[1:m0, ])),
+  y = c(perf[1:m0, ], perf[1:m0, ]),
   group = factor(rep(1:n0, each = m0)),
   version = rep(c("V4", "V3"), times = n0)
 )
 
 # Set up the plot
-p3b <-
-  ggplot(plot_data, aes(x = x, y = y, group = group, color = version)) +
-  geom_point(shape = 18, size = 3) +
+p3b1_min <-
+  ggplot(plot_data, aes(x = x, y = y, group = version)) +
+  geom_point(shape = 18, size = 3, aes(col = version)) +
   scale_x_discrete(breaks = (m0 + 2) * (1:n0) - floor(m0 / 2) - 1, labels = labs) +
-  scale_y_continuous(expand = c(0, 0), limits = c(0, 1)) +
+  #scale_y_continuous(expand = c(0, 0), limits = c(min(plot_data$y)-0.1, 0.9)) +
+  scale_color_manual(values = c("V4" = "black", "V3" = "red")) +
+  # scale_y_continuous(expand = c(0, 0), limits = c(0, 1)) +
   labs(x = "SIMD", y = "AUROC") +
   theme_bw() +
   theme(
@@ -67,6 +69,80 @@ p3b <-
   )
 
 
-ggsave("Figures/pdfs/Fig_3b.pdf", p3b,
+ggsave("Figures/pdfs/Fig_3b1.pdf", p3b1_min,
+       width = 8.5, height = 9, units = "cm",
+       device = cairo_pdf)
+
+p3b1_zero <-
+  ggplot(plot_data, aes(x = x, y = y, group = version)) +
+  geom_point(shape = 18, size = 3, aes(col = version)) +
+  scale_x_discrete(breaks = (m0 + 2) * (1:n0) - floor(m0 / 2) - 1, labels = labs) +
+  #scale_y_continuous(expand = c(0, 0), limits = c(min(plot_data$y)-0.1, 0.9)) +
+  scale_color_manual(values = c("V4" = "black", "V3" = "red")) +
+  scale_y_continuous(expand = c(0, 0), limits = c(0, 1)) +
+  labs(x = "SIMD", y = "AUROC") +
+  theme_bw() +
+  theme(
+    legend.position = "bottom",
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank()
+  )
+
+# ggsave("Figures/pdfs/Fig_3b1.pdf", p3b1_zero,
+#        width = 8.5, height = 9, units = "cm",
+#        device = cairo_pdf)
+
+
+# difference in AUC
+plot_data <- data.frame(
+  x = factor(rep((m0 + 2) * (1:n0) - floor(m0 / 2) - 1, each = 1)),
+  y = c(perf[1:m0, ], perf[1:m0, ]),
+  group = factor(rep(1:n0, each = m0)),
+  version = rep(c("V4", "V3"), times = n0)
+)
+
+plot_data$difference <- plot_data$y[plot_data$version == "V4"] -
+  plot_data$y[plot_data$version == "V3"]
+
+# Set up the plot
+p3b2 <-
+  ggplot(plot_data, aes(x= x, y = difference)) +
+  geom_point(shape = 18, size = 3) +
+  scale_x_discrete(labels = labs) +
+  xlab("SIMD") +
+  ylab("Difference in AUC") +
+  theme_bw() +
+  theme(
+    legend.position = "bottom",
+    legend.title = element_blank(),
+    axis.text.x = element_text(angle = 45, hjust = 1)
+  )
+
+ggsave("Figures/pdfs/Fig_3b2.pdf", p3b2,
+       width = 8.5, height = 9, units = "cm",
+       device = cairo_pdf)
+
+
+
+# Percentage increase
+plot_data$percentage_increase <- ((plot_data$y[plot_data$version == "V4"] / plot_data$y[plot_data$version == "V3"]) - 1) * 100
+
+
+p3b3 <-
+  ggplot(plot_data, aes(x = x, y = percentage_increase)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  xlab("SIMD") +
+  ylab("Percentage Increase") +
+  scale_x_discrete(labels = labs) +
+  theme_bw() +
+  theme(
+    legend.position = "bottom",
+    legend.title = element_blank(),
+    axis.text.x = element_text(angle = 45, hjust = 1)
+  )
+
+
+ggsave("Figures/pdfs/Fig_3b3.pdf", p3b3,
        width = 8.5, height = 9, units = "cm",
        device = cairo_pdf)
