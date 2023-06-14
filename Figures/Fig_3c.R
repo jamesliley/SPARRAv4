023ddb71c3988f764d75c2851e346db5fcb7b8c7
IoannaThoma <ioanna.thoma@ed.ac.uk>
2023-06-14 13:28:40 +0100

    V3 cohorts updated figures

--- Figures/Fig_3c.R
+++ Figures/Fig_3c.R
@@ -54,81 +54,53 @@ se <- c(0.0005187255, 0.0003386057, 0.0004669980, 0.0008822474,
         0.0005326287, 0.0003475607, 0.0004784836, 0.0008933571)
 plot_data$se <- se
 # Set up the plot
-p3c1_min <-
+p3c1 <-
   ggplot(plot_data, aes(x = x, y = y, group = version)) +
   geom_point(aes(color = version), shape = 18, size = 2) +
   geom_errorbar(aes(ymin = y - 3*se, ymax = y + 3*se, col = version), width = 0.2, size = 0.4) + # position = position_dodge(width = 1)
 
   scale_color_manual(values = c("V4" = "black", "V3" = "red")) +
   scale_x_discrete(
-    breaks = (m0 + 2) * (1:n0) - floor(m0 / 2) - 1,
+    #breaks = (m0 + 2) * (1:n0) - floor(m0 / 2) - 1,
     labels = labs,
     #expand = c(0, 0), limits = c(0, 15),  # Adjust the expand argument to extend the x-axis limits
     position = "bottom"
   ) +
-  #scale_y_continuous(expand = c(0, 0), limits = c(0, 1)) +  # Extend the y-axis to 1
+  scale_y_continuous(expand = c(0, 0), limits = c(0.71, 0.81)) +
   labs(x = "Cohort", y = "AUROC") +
   guides(col = guide_legend(title = NULL)) +
-  coord_fixed(ratio = 50) +
+  coord_fixed(ratio = 60) +
   theme_bw() +
   theme(
     legend.position = "none",
-    axis.text.x = element_text(angle = 0, hjust = 1, size = 7),
+    axis.text.x = element_text(size = 8),
     panel.grid.major.y = element_blank(),
     panel.grid.minor.y = element_blank()
   )
 
 
-ggsave("Figures/pdfs/Fig_3c1.pdf", p3c1_min,
+ggsave("Figures/pdfs/Fig_3c1.pdf", p3c1,
        width = 8.5, height = 9, units = "cm",
        device = cairo_pdf)
 
 
-# difference in AUC
-plot_data <- data.frame(
-  x = factor(rep((m0 + 2) * (1:n0) - floor(m0 / 2) - 1, each = 1)),
-  y = c(perf[1:m0, ], perf[1:m0, ]),
-  group = factor(rep(1:n0, each = m0)),
-  version = rep(c("V4", "V3"), times = n0)
+plot_data2 <- data.frame(
+  x = factor(seq_len(ncol(perf))),
+  difference = plot_data$y[plot_data$version == "V4"] -
+    plot_data$y[plot_data$version == "V3"],
+  percentage_increase = ((plot_data$y[plot_data$version == "V4"] / plot_data$y[plot_data$version == "V3"]) - 1) * 100,
+  prop_events = xrate
 )
-plot_data$difference <- plot_data$y[plot_data$version == "V4"] -
-  plot_data$y[plot_data$version == "V3"]
 
-# Set up the plot
+# create a plot that shows the AUC differences as a percentage increase (v4 vs v3)
 p3c2 <-
-  ggplot(plot_data, aes(x = x, y = difference, group = version)) +
-  geom_point(shape = 18, size = 3) +
-  scale_x_discrete(
-    breaks = (m0 + 2) * (1:n0) - floor(m0 / 2) - 1,
-    labels = labs,
-    position = "bottom") +
-  xlab("Cohort") +
-  ylab("Difference in AUC") +
-  theme_bw() +
-  theme(
-    legend.position = "bottom",
-    legend.title = element_blank(),
-    axis.text.x = element_text(angle = 45, hjust = 1)
-  )
-
-ggsave("Figures/pdfs/Fig_3c2.pdf", p3c2,
-       width = 8.5, height = 9, units = "cm",
-       device = cairo_pdf)
-
-
-
-# Percentage increase
-plot_data$percentage_increase <- ((plot_data$y[plot_data$version == "V4"] / plot_data$y[plot_data$version == "V3"]) - 1) * 100
-
-
-p3c3 <-
-  ggplot(plot_data, aes(x = x, y = percentage_increase)) +
+  ggplot(plot_data2, aes(x = x, y = percentage_increase)) +
   geom_bar(stat = "identity", position = "dodge", width = 0.5) +
   xlab("Cohort") +
-  ylab("Percentage Increase") +
+  ylab("% increase") +
   scale_fill_manual(values = c("V4" = "black", "V3" = "red")) +
   scale_x_discrete(labels = labs) +
-  coord_fixed(ratio = 1.5) + # Set aspect ratio to 1:1
+  coord_fixed(ratio = 0.5) + # Set aspect ratio to 1:1
   theme_bw() +
   theme(
     legend.position = "bottom",
@@ -136,42 +108,42 @@ p3c3 <-
     panel.grid.major.y = element_blank(),
     panel.grid.minor.y = element_blank(),
     panel.grid.minor.x = element_blank(),
-    axis.text.x = element_text(angle = 0, hjust = 1, size = 7)
-  )
+    axis.text.x = element_text(size = 8)
+  ) +
+  ylim(0, 4.4)
 
-
-ggsave("Figures/pdfs/Fig_3c3.pdf", p3c3,
-       width = 8.5, height = 9, units = "cm",
-       device = cairo_pdf)
-
-
-
-diffs <- data.frame(
-  x = (m0 + 2)*(-0.5 + 1:n0) - 0.5*(m0 + 2)/n0,
-  y = round(xrate, 3),
-  group = factor(rep(1, n0))
-)
-
-freq <-ggplot(diffs, aes(x = x, y = y, group = group)) +
+# create a plot that shows the AUC differences as a percentage increase (v4 vs v3)
+p3c3 <-
+  ggplot(plot_data2, aes(x = x, y = prop_events)) +
   geom_bar(stat = "identity", position = "dodge", width = 0.5) +
-  scale_x_continuous(limits = c(0, n0*(m0 + 2)),
-                     breaks = (m0 + 2)*(1:n0) - floor(m0/2) - 1 ,
-                     labels = labs) +
-  scale_y_continuous(limits = c(0, max(diffs$y) + 0.01),
-                     breaks = seq(0, max(diffs$y), length.out = 5),
-                     labels = function(x) round(x, 2),
-                     expand = expansion(mult = c(0.0000005, 0.00000005))) +
-  labs(x = "Cohort", y = "EA Frequency") +
-  coord_fixed(ratio = 50) +  # Set aspect ratio to 1:1
+  xlab("Cohort") +
+  ylab("% events") +
+  scale_fill_manual(values = c("V4" = "black", "V3" = "red")) +
+  scale_x_discrete(labels = labs) +
+  coord_fixed(ratio = 7) + # Set aspect ratio to 1:1
   theme_bw() +
   theme(
+    legend.position = "bottom",
+    legend.title = element_blank(),
     panel.grid.major.y = element_blank(),
     panel.grid.minor.y = element_blank(),
     panel.grid.minor.x = element_blank(),
-    axis.text.x = element_text(angle = 0, hjust = 1, size = 7)
-  )
+    axis.text.x = element_text(size = 8)
+  ) +
+  ylim(0, 0.33)
 
+p3c <- ggarrange( p3c1 + theme(axis.text.x = element_blank(),
+                               axis.ticks.x = element_blank(),
+                               axis.title.x = element_blank(),
+                               plot.margin = margin(t = 0.5, r = 0.2, b = 0, l = 0.2, unit = "cm")),
+                  p3c2 + theme(axis.text.x = element_blank(),
+                               axis.ticks.x = element_blank(),
+                               axis.title.x = element_blank(),
+                               plot.margin = margin(t = 0, r = 0.2, b = 0, l = 0.2, unit = "cm")),
+                  p3c3 + theme(plot.margin = margin(t = 0, r = 0.2, l = 0.2, unit = "cm")),
+                  nrow = 3)
 
-ggsave("Figures/pdfs/Fig_3c4.pdf", freq,
-       width = 8.5, height = 9, units = "cm",
+ggsave("Figures/pdfs/Fig_3c.pdf", p3c,
+       width = 10, height = 20, units = "cm",
        device = cairo_pdf)
+
