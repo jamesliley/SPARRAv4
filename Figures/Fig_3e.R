31233b9ba2a4feb413ec1acd59c73b1f735566b7
James Liley <james.liley@xtra.co.nz>
2023-11-13 14:53:11 +0000

    Changes to violin plot in response to reviewer comment A18

--- Figures/Fig_3e.R
+++ Figures/Fig_3e.R
@@ -8,31 +8,56 @@ eval(import_sparra_expr(paste0(plot_dir,"Analytics/disease_class_violin.txt")))
 # old code
 pdf(paste0(plot_dir,"Analytics/disease_class_violin.pdf"),width=6,height=5)
 
+col1="blue" # Colour for categories
+
 par(mar=c(8.1,4.1,4.1,2.1))
 dmax=0
-for (i in 1:length(xmed)) {
+for (i in c(1:length(xmed))) {
   dz=get(paste0("d",i))
   dmax=c(dmax,max(dz$y))
 }
+space1=4/3
+space2=1
+
 sc=0.3/max(dmax)
 
-plot(0,type="n",xlim=c(0,2+length(xl0)),ylim=c(0,2),bty="n",xaxt="n",yaxt="n",
+plot(0,type="n",xlim=c(0,4*space1 + length(xmed)*space2),ylim=c(0,2),bty="n",xaxt="n",yaxt="n",yaxs="i",
      xlab="",ylab="Score (%), log scale")
 
-polygon(c(sc*d0$y,rev(-sc*d0$y))+1/3,c(d0$x,rev(d0$x)),col="blue",border=NA)
-points(1/3,xmed0,pch=16)
-
-polygon(c(sc*dx$y,rev(-sc*dx$y))+5/3,c(dx$x,rev(dx$x)),col="blue",border=NA)
-points(5/3,xmedx,pch=16)
+locs=c()
+for (i in 1:4) {
+  suff = c("0","x","eb","d")[i]
+  
+  d=get(paste0("d",suff))
+  m=get(paste0("xmed",suff))
+  polygon(c(sc*d$y,rev(-sc*d$y))+(i-0.5)*space1,c(d$x,rev(d$x)),col=col1,border=NA)
+  points((i-0.5)*space1,m,pch=16)
+  locs=c(locs,(i-0.5)*space1)
+}
+ofs=(i+0.5)*space1
 
 ox=order(-xmed)
 for (i0 in 1:length(xl0)) {
   i=ox[i0]
   dz=get(paste0("d",i))
-  polygon(c(sc*dz$y,rev(-sc*dz$y))+i0+2,c(dz$x,rev(dz$x)),col="gray",border=NA)
-  points(i0+2,xmed[i],pch=16)
+  polygon(c(sc*dz$y,rev(-sc*dz$y))+ofs + (i0-0.5)*space2,c(dz$x,rev(dz$x)),col="gray",border=NA)
+  points(ofs + (i0-0.5)*space2,xmed[i],pch=16)
+  locs=c(locs,ofs + (i0-0.5)*space2)
 }
-axis(1,at=c(1/3,5/3,3:(2+length(xl0))),labels=c("Not admitted","All admitted",xl0[ox]),las=2,cex.axis=0.5)
 yp=pretty(c(0,2),n=5)
 axis(2,at=yp,labels=round((10^yp)),las=2,cex.axis=0.5)
+axis(1,at=locs[1:4],
+     labels=c("No admission/death","All admitted/died","Admitted","Died"),
+     col=col1, # suppress line
+     col.ticks=col1,
+     col.axis=col1,
+     las=2,cex.axis=0.5)
+axis(1,at=locs[5:length(locs)],
+     labels=xl0[ox],
+     col="black", # suppress line
+     col.ticks="black",
+     col.axis="black",
+     las=2,cex.axis=0.5)
+
+
 dev.off()
