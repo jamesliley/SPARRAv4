00ac7f2abb3400355404a0c8b08b2b8c7dd45297
James Liley <james.liley@xtra.co.nz>
2023-11-13 13:44:00 +0000

    Forgot one plot in last commit

--- Figures/Fig_3f.R
+++ Figures/Fig_3f.R
@@ -1,6 +1,6 @@
 # Pipelines/main_pipeline.R
 source("Figures/util.R") # for import_sparra_expr()
-plot_dir="~/SPARRAv4/Analysis/full_model/"
+plot_dir="Analysis/full_model/"
 # To recreate: first set
 x=c(1,1); tmax=1
 
@@ -20,9 +20,9 @@ plot(0,type="n",xlim=c(0,365),xlab="Days after time cutoff",ylim=c(0,mh),
 
 gcol=gray((1:xmax)/(1.5*xmax))
 for (i in 1:xmax) lines(get(paste0("d",i)),col=gcol[i],lty=1,lwd=2)
-lines(d0,col="red",lwd=2)
+lines(d0,col="blue",lwd=2)
 
-legend("topright",lty=1,lwd=2,col=c(gcol[1],"white",gcol[xmax],"red"),
+legend("topright",lty=1,lwd=2,col=c(gcol[1],"white",gcol[xmax],"blue"),
        c(expression(paste(hat(P),"(EA)>0.1")),"...",bquote(paste(hat(P),"(EA)>",.(xmax/10))),"All"))
 
 dev.off()
