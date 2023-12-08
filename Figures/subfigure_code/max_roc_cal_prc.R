b42f04d901c3805a07ec3d709bd1100d8da15a99
James Liley <james.liley@xtra.co.nz>
2023-12-08 12:17:21 +0000

    Added v4 lines to cal/roc plots for max(v3,v4)

--- Figures/subfigure_code/max_roc_cal_prc.R
+++ Figures/subfigure_code/max_roc_cal_prc.R
@@ -14,9 +14,12 @@ labs=c(paste0("v3: ",auc3," (",se3,")"),paste0("v4: ",auc4," (",se4,")"),
 #roc_2panel_gg(list(xroc3b,xroc4b,xrocmb),labels=labs,
 #           col=c("blue","black","red"),xy_col="gray",
 #           legend_title="AUROC (SE)")
-roc_2panel_gg(list(xrocmb),labels=labs[3],
-           col=c("blue","black","red")[2],xy_col="gray",
-           legend_title="AUROC (SE)")
+#roc_2panel_gg(list(xrocmb),labels=labs[3],
+#           col=c("blue","black","red")[2],xy_col="gray",
+#           legend_title="AUROC (SE)")
+roc_2panel_gg(list(xroc4b,xrocmb),labels=labs[2:3],
+              col=c("red","black"),xy_col="gray",
+              legend_title="AUROC (SE)")
 dev.off()
 
 
@@ -31,7 +34,9 @@ labs=c(paste0("v3: ",auc3," (",se3,")"),paste0("v4: ",auc4," (",se4,")"),
        paste0("Max: ",aucm," (",sem,")"))
 #prc_2panel_gg(list(xprc3b,xprc4b,xprcmb),labels=labs,col=c("blue","red","black"),
 #           legend_title="AUPRC (SE)")
-prc_2panel_gg(list(xprcmb),labels=labs[3],col=c("black"),
+#prc_2panel_gg(list(xprcmb),labels=labs[3],col=c("black"),
+#           legend_title="AUPRC (SE)")
+prc_2panel_gg(list(xprc4b,xprcmb),labels=labs[2:3],col=c("red","black"),
            legend_title="AUPRC (SE)")
 dev.off()
 
@@ -44,6 +49,8 @@ labs=c("v3", "v4","Max")
 cic=c(rgb(0,0,1,alpha=0.5),rgb(1,0,1,alpha=0.5),rgb(0.5,0.5,0.5,alpha=0.5))
 #cal_2panel_gg(list(xcal3,xcal4,xcalm),labels=labs,col=c("blue","red","black"),
 #             ci_col=cic,xy_col="gray")
-cal_2panel_gg(list(xcalm),labels=labs[3],col=c("blue","red","black")[3],
-             ci_col=cic[3],xy_col="gray")
+#cal_2panel_gg(list(xcalm),labels=labs[3],col=c("blue","red","black")[3],
+#             ci_col=cic[3],xy_col="gray")
+cal_2panel_gg(list(xcal4,xcalm),labels=labs[2:3],col=c("red","black"),
+             ci_col=cic[2:3],xy_col="gray")
 dev.off()
